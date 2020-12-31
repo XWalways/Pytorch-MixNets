fa946fd01ba4b84483fe056cd422df9db6780c9e
肖伟 <718309617@qq.com>
2020-12-31 13:02:24 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py -ms s --data /home/data/.mxnet/datasets/imagenet --epochs 100 --lr-decay 0.1 --lr-mode multistep --lr-decay-epoch 30,60,90 -c checkpoints/mixnet --logdir ./logs/mixnet --gpu-id 0,1
+python train.py -ms s --data /home/data/.mxnet/datasets/imagenet --epochs 100 --lr-decay 0.1 --lr-mode multistep --lr-decay-epoch 30,60,90 -c checkpoints/mixnet --logdir ./logs/mixnet --gpu-id 0,1,2,3,4,5,6,7
